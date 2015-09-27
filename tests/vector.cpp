9afaf7463fa10b231dbde63b0fc25deaa071a119
ga2arch <gabriele.carrettoni@gmail.com>
2015-09-27 18:38:04 +0200

    testing commit

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -10,7 +10,5 @@ SCENARIO( "vectors manipulation", "[vector]" ) {
         Vector<4> vec({0.0f,1.1f,2.0f,3.0f});
         
         REQUIRE( vec.data().size() == 4 );
-        
-                
     }
 }
